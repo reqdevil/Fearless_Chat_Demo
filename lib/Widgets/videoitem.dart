325101e882312bcfa12c6cb33ea8ea5aa07ed67c
Armagan <armagan4celik@gmail.com>
2022-01-04 15:19:40 +0300

    Update videoitem.dart
    videoitem play icon stiuation updated

--- lib/Widgets/videoitem.dart
+++ lib/Widgets/videoitem.dart
@@ -5,7 +5,7 @@ import 'package:video_player/video_player.dart';
 class VideoItem extends StatefulWidget {
   final String url;
 
- const VideoItem({Key? key, required this.url}) : super(key: key);
+  const VideoItem({Key? key, required this.url}) : super(key: key);
   @override
   _VideoItemState createState() => _VideoItemState();
 }
@@ -21,8 +21,25 @@ class _VideoItemState extends State<VideoItem> {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
       });
+    _controller.addListener(() {
+      // if (_controller.value.position ==
+      //     const Duration(seconds: 0, minutes: 0, hours: 0)) {
+      //   // print('video Started');
+      //   setState(() {
+      //     _isPlaying = false;
+      //   });
+      // }
+
+      if (_controller.value.position == _controller.value.duration) {
+        // print('video Ended');
+        setState(() {
+          _isPlaying = false;
+        });
+      }
+    });
   }
 
+  bool _isPlaying = false;
   @override
   Widget build(BuildContext context) {
     return _controller.value.isInitialized
@@ -35,7 +52,8 @@ class _VideoItemState extends State<VideoItem> {
                 alignment: Alignment.bottomLeft,
                 child: GestureDetector(
                     onTap: playPause,
-                    child: Icon(Icons.play_circle,
+                    child: Icon(
+                        _isPlaying ? Icons.pause_circle : Icons.play_circle,
                         color: Colors.white.withOpacity(0.6))))
           ])
         : Container();
@@ -49,8 +67,14 @@ class _VideoItemState extends State<VideoItem> {
 
   playPause() {
     if (_controller.value.isPlaying) {
+      setState(() {
+        _isPlaying = false;
+      });
       _controller.pause();
     } else {
+      setState(() {
+        _isPlaying = true;
+      });
       _controller.play();
     }
   }
