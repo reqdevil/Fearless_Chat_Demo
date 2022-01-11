ae1479a75cbf35a704dd0420b3664e650ff1c556
Armagan <armagan4celik@gmail.com>
2022-01-11 12:00:53 +0300

    Update mainPage.dart

--- lib/Pages/mainPage.dart
+++ lib/Pages/mainPage.dart
@@ -542,11 +542,4 @@ class _PlaceholderWidgetState extends State<PlaceholderWidget> {
   }
 }
 
-extension Unique<E, Id> on List<E> {
-  List<E> unique([Id Function(E element)? id, bool inplace = true]) {
-    final ids = Set();
-    var list = inplace ? this : List<E>.from(this);
-    list.retainWhere((x) => ids.add(id != null ? id(x) : x as Id));
-    return list;
-  }
-}
+
