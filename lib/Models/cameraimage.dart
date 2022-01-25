8e18e3e82512ded4bc2d760db391f3fd49ef7f35
Armagan <armagan4celik@gmail.com>
2022-01-25 18:08:43 +0300

    Update cameraimage.dart

--- lib/Models/cameraimage.dart
+++ lib/Models/cameraimage.dart
@@ -1,11 +1,13 @@
+import 'package:photo_gallery/photo_gallery.dart';
+
 class TakenCameraMedia {
   String filePath;
   bool isSelected;
   DateTime dateTime;
   FileType fileType;
-
-  TakenCameraMedia(
-      this.filePath, this.isSelected, this.dateTime, this.fileType);
+  Medium? medium;
+  TakenCameraMedia(this.filePath, this.isSelected, this.dateTime, this.fileType,
+      [this.medium = null]);
 }
 
 enum FileType { photo, video }
